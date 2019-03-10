8c09fc130d09d7aa69c6ad1cadc4231802bfc146
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-03-09 20:20:11 -0500

    cleaned up javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ var spot8 = document.getElementById("spot8");
 var spot9 = document.getElementById("spot9");
 
 var guessesDiv = document.getElementById("guesses-div");
+var textChanger = document.getElementById("text-changer");
 
 var spotArray = [
   spot1,
@@ -23,23 +24,24 @@ var spotArray = [
 ];
 
 var wordArray = [
-  "R",
-  "A",
-  "Y",
-  "M",
-  "O",
-  "N",
-  "D"
-];
+  ["C", "H", "E", "E", "S", "E"],
+  ["C", "R", "U", "S", "T"],
+  ["S", "A", "U", "C", "E"],
+  ["P", "E", "P", "P", "E", "R", "O", "N", "I"],
+  ["D", "O", "U", "G", "H"]
+]
 
 var guesses = 15;
+var currentWordArray = wordArray[Math.round(Math.random() * 3)]
 
-console.log(wordArray.length)
+console.log(currentWordArray.length)
+console.log(currentWordArray);
+
 
 function setSpaces() {
-  console.log("Word length: " + wordArray.length)
+  console.log("Word length: " + currentWordArray.length)
 
-  if (wordArray.length >= 1) {
+  if (currentWordArray.length >= 1) {
     console.log("wordLength >= 1")
     spot1.style.display = "inline";
   } else {
@@ -47,7 +49,7 @@ function setSpaces() {
     spot1.style.display = "none";
   }
 
-  if (wordArray.length >= 2) {
+  if (currentWordArray.length >= 2) {
     console.log("wordLength >= 2")
     spot2.style.display = "inline";
   } else {
@@ -55,7 +57,7 @@ function setSpaces() {
     spot2.style.display = "none";
   }
 
-  if (wordArray.length >= 3) {
+  if (currentWordArray.length >= 3) {
     console.log("wordLength >= 3")
     spot3.style.display = "inline";
   } else {
@@ -63,7 +65,7 @@ function setSpaces() {
     spot3.style.display = "none";
   }
 
-  if (wordArray.length >= 4) {
+  if (currentWordArray.length >= 4) {
     console.log("wordLength >= 4")
     spot4.style.display = "inline";
   } else {
@@ -71,7 +73,7 @@ function setSpaces() {
     spot4.style.display = "none";
   }
 
-  if (wordArray.length >= 5) {
+  if (currentWordArray.length >= 5) {
     console.log("wordLength >= 5")
     spot5.style.display = "inline";
   } else {
@@ -79,7 +81,7 @@ function setSpaces() {
     spot5.style.display = "none";
   }
 
-  if (wordArray.length >= 6) {
+  if (currentWordArray.length >= 6) {
     console.log("wordLength >= 6")
     spot6.style.display = "inline";
   } else {
@@ -87,7 +89,7 @@ function setSpaces() {
     spot6.style.display = "none";
   }
 
-  if (wordArray.length >= 7) {
+  if (currentWordArray.length >= 7) {
     console.log("wordLength >= 7")
     spot7.style.display = "inline";
   } else {
@@ -95,7 +97,7 @@ function setSpaces() {
     spot7.style.display = "none";
   }
 
-  if (wordArray.length >= 8) {
+  if (currentWordArray.length >= 8) {
     console.log("wordLength >= 8")
     spot8.style.display = "inline";
   } else {
@@ -103,39 +105,75 @@ function setSpaces() {
     spot8.style.display = "none";
   }
 
-  if (wordArray.length >= 9) {
+  if (currentWordArray.length >= 9) {
     console.log("wordLength >= 9")
     spot9.style.display = "inline";
   } else {
     console.log("wordLength <= 9")
     spot9.style.display = "none";
   }
-
+  return
 }
 
 function guessLetter(letterGuess) {
 
   var letterGuess = letterGuess.toUpperCase();
 
-  if (wordArray.includes(letterGuess)) {
+  if (currentWordArray.includes(letterGuess)) {
     console.log("Word includes " + letterGuess)
-    var letterIndex = wordArray.indexOf(letterGuess)
+    var letterIndex = currentWordArray.indexOf(letterGuess)
     console.log("Index of " + letterGuess + " is " + letterIndex)
-    spotArray[letterIndex].textContent = wordArray[letterIndex];
+    spotArray[letterIndex].textContent = currentWordArray[letterIndex];
   } else {
     console.log("Word does not include " + letterGuess)
     guesses--;
   }
-    guessesDiv.textContent = guesses;
-    /* SUBTRACT POINT/ADD LETTER TO GUESSED */
+  for (i=0; i <= currentWordArray.length; i++) {
 
+    var blankSpots = 0;
+
+  if (spotArray[i].textContent === "_") {
+    blankSpots++;
+  }
+
+  if (blankSpots = currentWordArray.length) {
+    textChanger.textContent = "YOU WIN!"
+  }
+  console.log("Blank spots: " + blankSpots)
+}
+    guessesDiv.textContent = guesses;
+    letterListen();
 }
 
 setSpaces();
+letterListen();
 guessesDiv.textContent = guesses;
 
-if(guesses >= 0) {
+
+function letterListen() {
+if (guesses > 0) {
+  console.log("Guesses is over 0.")
   document.onkeyup = function(event) {
     guessLetter(event.key);
   }
+} else {
+  console.log("No more guesses.")
+  gameOver();
+}
+}
+
+function gameOver() {
+   spotArray[0].textContent = currentWordArray[0];
+   spotArray[1].textContent = currentWordArray[1]
+   spotArray[2].textContent = currentWordArray[2]
+   spotArray[3].textContent = currentWordArray[3]
+   spotArray[4].textContent = currentWordArray[4]
+   spotArray[5].textContent = currentWordArray[5]
+   spotArray[6].textContent = currentWordArray[6]
+   spotArray[7].textContent = currentWordArray[7]
+   spotArray[8].textContent = currentWordArray[8]
+   textChanger.textContent = "GAME OVER!";
+
+
+    console.log("changed!");
 }
